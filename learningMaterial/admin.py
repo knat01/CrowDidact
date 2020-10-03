9726ec898622d62363ec6329e9e9140c45fde16f
Raynor Elgie <elgieraynor@gmail.com>
2020-10-03 12:13:39 -0400

    Add models to admin, add Subject

--- learningMaterial/admin.py
+++ learningMaterial/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import LectureNote,YoutubeVideo,Blurb,Subject
 
 # Register your models here.
+admin.site.register(LectureNote)
+admin.site.register(YoutubeVideo)
+admin.site.register(Blurb)
+admin.site.register(Subject)
--- learningMaterial/models.py
+++ learningMaterial/models.py
@@ -3,16 +3,21 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Subject(models.Model):
+    name = models.CharField(max_length=150)
+
 class Blurb(models.Model):
     blurbText = models.TextField(max_length=5000)
     fromLink = models.URLField(max_length=600)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, null=True)
 
 class YoutubeVideo(models.Model):
     link = models.URLField(max_length=600)
     favorites = models.IntegerField()
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, null=True)
 
 class LectureNote(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField()
     favorites = models.IntegerField()
-
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, null=True)
