148140c9c343608dcfc47d18af76a24cc7e78b11
Raynor Elgie <elgieraynor@gmail.com>
2020-10-03 15:05:34 -0400

    __str__ method, image on testpage

--- learningMaterial/models.py
+++ learningMaterial/models.py
@@ -6,15 +6,23 @@ from django.contrib.auth.models import User
 class Subject(models.Model):
     name = models.CharField(max_length=150)
 
+    def __str__(self):
+        return self.name
+
 class Blurb(models.Model):
     blurbText = models.TextField(max_length=5000)
     fromLink = models.URLField(max_length=600)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name="blurb")
+    def __str__(self):
+        return self.subject
 
 class YoutubeVideo(models.Model):
     link = models.URLField(max_length=600)
     favorites = models.IntegerField()
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.link
 
 class LectureNote(models.Model):
     title = models.CharField(max_length=80,default="Note")
@@ -22,3 +30,6 @@ class LectureNote(models.Model):
     image = models.ImageField()
     favorites = models.IntegerField()
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.image.url
