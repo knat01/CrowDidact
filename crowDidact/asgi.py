561faf71fb97d7a52c9baf1ca14aeefe8e9aa766
Asy <60577788+System-out-print-Asgar@users.noreply.github.com>
2020-10-03 16:15:26 -0400

    Update asgi.py

--- crowDidact/asgi.py
+++ crowDidact/asgi.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 """
 ASGI config for crowDidact project.
@@ -33,3 +34,40 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crowDidact.settings')
 
 application = get_asgi_application()
 >>>>>>> 4bff61d89d9cce8713ec1b06cbf243ff008e06d8
+=======
+<<<<<<< HEAD
+"""
+ASGI config for crowDidact project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crowDidact.settings')
+
+application = get_asgi_application()
+=======
+"""
+ASGI config for crowDidact project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crowDidact.settings')
+
+application = get_asgi_application()
+>>>>>>> 4bff61d89d9cce8713ec1b06cbf243ff008e06d8
+>>>>>>> Stashed changes
