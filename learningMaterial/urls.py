1ca12f2b8525aa7e7e3208c1d63c9a58dcab5f5a
Raynor Elgie <elgieraynor@gmail.com>
2020-10-03 17:34:57 -0400

    programmatic urls & more examples

--- learningMaterial/urls.py
+++ learningMaterial/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.subject, name='subject'),
+    path('subjects/<str:subjectStr>/', views.subject, name='subjectPlus'),
     path('upload/', views.upload, name='upload'),
 ]
 
