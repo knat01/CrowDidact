1a841e30fb5b4e258d6f0b25b25d3391246d5798
Raynor Elgie <elgieraynor@gmail.com>
2020-10-03 14:44:10 -0400

    test subjectBase working to display attributes

--- learningMaterial/models.py
+++ learningMaterial/models.py
@@ -9,16 +9,16 @@ class Subject(models.Model):
 class Blurb(models.Model):
     blurbText = models.TextField(max_length=5000)
     fromLink = models.URLField(max_length=600)
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, null=True, related_name="blurb")
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name="blurb")
 
 class YoutubeVideo(models.Model):
     link = models.URLField(max_length=600)
     favorites = models.IntegerField()
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, null=True)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
 
 class LectureNote(models.Model):
     title = models.CharField(max_length=80,default="Note")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField()
     favorites = models.IntegerField()
-    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, null=True)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
