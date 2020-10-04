3cefc21f31d48e346ba2cc12b4d238e0c2b7d4bd
Raynor Elgie <elgieraynor@gmail.com>
2020-10-03 21:37:02 -0400

    fixed error from adding validator

--- learningMaterial/models.py
+++ learningMaterial/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core import validators
+from django.core.validators import RegexValidator
 
 # Create your models here.
 
