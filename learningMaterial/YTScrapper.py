f4c5416e726f504981d4408426e236523283abce
Asy <60577788+System-out-print-Asgar@users.noreply.github.com>
2020-10-03 22:36:33 -0400

    Update YTScrapper.py

--- learningMaterial/YTScrapper.py
+++ learningMaterial/YTScrapper.py
@@ -9,45 +9,26 @@ API_KEY = 'AIzaSyCmcZAUUECqzVu3QzP1oO3YiSfZvMddqmM'
 youtube = build('youtube' , 'v3' , developerKey=API_KEY)
 #Build object using Key
 
-
-Topic = input('Enter the topic to search for: ')
-Range = int(input('Enter the number of links to search for: '))
-#choose range and element
+def YTScrape (Topic, AMT):
 
 
-request = youtube.search().list( q=Topic , part='snippet' , type='video', videoEmbeddable='true' , maxResults=Range )
-#API request
+    Topic = Topic
+    Range = int(AMT)
+    #choose range and element
 
 
-response = request.execute()
-#return response from API server
+    request = youtube.search().list( q=Topic , part='snippet' , type='video', videoEmbeddable='true' , maxResults=Range )
+    #API request
 
 
-for item in response['items']:
-    print('')
-    print('Video name: '+ item['snippet']['title'])
-    print('Embed url: '+ 'https://www.youtube.com/embed/'+item['id']['videoId'])
-    print('')
-#output
+    response = request.execute()
+    #return response from API server
 
-while (Range != 0):
-#Walmart brand do while loop
 
-    Topic = input('Enter the topic to search for: ')
-    Range = int(input('Enter the number of links to search for: '))
-
-    if(Range == 0 or Topic == '0'):
-        exit()
-        #exit condition
-    
-    request2 = youtube.search().list( q=Topic , part='snippet' , type='video', videoEmbeddable='true' , maxResults=Range )
-
-    response2 = request2.execute()
-    
-    for item in response2['items']:
+    for item in response['items']:
         print('')
         print('Video name: '+ item['snippet']['title'])
         print('Embed url: '+ 'https://www.youtube.com/embed/'+item['id']['videoId'])
         print('')
+    #output
 
-    
